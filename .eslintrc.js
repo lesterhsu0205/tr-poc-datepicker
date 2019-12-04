1b814b472b89f67b690001ac3d82da9d00869aef
lesterhsu <lesterhsu0205@hotmail.com>
2019-12-04 14:08:07 +0800

    first build

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jquery: true,
+  },
+
+  rules: {
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'vue/no-unused-components': 'off',
+    'vue/this-in-template': 'off',
+    // 直接使用 prettier 不然會 loop 兩邊設定
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+        'semi': false
+      }
+    ]
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    // eslint-plugin-prettier 把 prettier cmd 加入到 eslint
+    'plugin:prettier/recommended',
+    // eslint-config-prettier 防止 eslint 與 prettier 相衝，需放最後
+    'prettier/vue'
+  ]
+}
---
+++ vue.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+    // https: true
+  },
+
+  // module: {
+  //     rules: [{
+  //         test: /\.scss$/,
+  //         use: [
+  //             "style-loader", // 将 JS 字符串生成为 style 节点
+  //             "css-loader", // 将 CSS 转化成 CommonJS 模块
+  //             "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
+  //         ]
+  //     }]
+  // },
+  // css: {
+  //     loaderOptions: {
+  //         sass: {
+  //             data:
+  //                 '@import "node_modules/vue-snotify/styles/material.scss";'
+  //         }
+  //     }
+  // },
+  configureWebpack: {
+    // devtool: 'source-map',
+    plugins: [
+      new webpack.ProvidePlugin({
+        // $: 'jquery',
+        // jQuery: 'jquery',
+        // jquery: 'jquery',
+        // 'window.jQuery': 'jquery',
+        // axios: 'axios',
+        // moment: 'moment',
+        // Chart: 'chart.js'
+      })
+    ]
+  }
+}
